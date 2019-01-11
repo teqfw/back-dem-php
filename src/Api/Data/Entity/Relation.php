6e0ff0a09fd7ba69dbcd8f9dab6f363fe101ae7a
Alex Gusev <alex@flancer64.com>
2019-01-11 12:38:49 +0200

    pytonizer-1 Multiple attribute values

--- src/Api/Data/Entity/Relation.php
+++ src/Api/Data/Entity/Relation.php
@@ -10,5 +10,34 @@ namespace TeqFw\Lib\Dem\Api\Data\Entity;
 class Relation
     extends \TeqFw\Lib\Data
 {
-
+    /**
+     * Ordered set of foreign entity attributes to be bound to the own attributes.
+     *
+     * @var string[]
+     */
+    public $foreignAttrs;
+    /**
+     * Ordered set of own attributes to be bound to the foreign entity attributes.
+     *
+     * @var string[]
+     */
+    public $ownAttrs;
+    /**
+     * 'On Update' actions.
+     *
+     * @var string
+     */
+    public $onUpdate;
+    /**
+     * 'On Delete' actions.
+     *
+     * @var string
+     */
+    public $onDelete;
+    /**
+     * Full path to foreign entity.
+     *
+     * @var string
+     */
+    public $pathToForeign;
 }
